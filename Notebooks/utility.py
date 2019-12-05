b0b371790770a34239701cef7f6683ab2bb88c95
Vinh <qvinh@live.ca>
2019-12-05 11:13:52 -0500

    Returned scores in cross val

--- Notebooks/utility.py
+++ Notebooks/utility.py
@@ -16,7 +16,7 @@ def plot_confusion_matrix(y_true: np.ndarray, y_pred: np.ndarray, classes: list,
     ax.set(xlabel = 'Predicted', ylabel = 'Actual')
 
     
-# Prints the cross validated scores
+# Prints the cross validated scores and returns the mean score across each metric
 def cross_validate_scores(clf, X: np.ndarray, y: np.ndarray, cv: int = 3, metrics: list =  ['accuracy']) -> None:
     scores_raw = cross_validate(clf, X, y,
                                 scoring = metrics,
@@ -24,35 +24,45 @@ def cross_validate_scores(clf, X: np.ndarray, y: np.ndarray, cv: int = 3, metric
                                 cv = cv,
                                 return_train_score = True,
                                 verbose = True)   
-    
+    train_scores = []
     print('Training scores')
     for metric in metrics:
         score = scores_raw['train_' + metric]
+        train_scores.append(score.mean())
         print('{}: {:.4f} ({:.4f})'.format(metric, score.mean(), score.std()))
-        
+    
+    val_scores = []
     print('\nValidation Scores')
     for metric in metrics:
         score = scores_raw['test_' + metric]
+        val_scores.append(score.mean())
         print('{}: {:.4f} ({:.4f})'.format(metric, score.mean(), score.std()))
     
+    return pd.DataFrame({'metric': metrics, 'train_score': train_scores, 'val_scores': val_scores})
 
 # Prints the best scores from the grid search results
-def print_best_grid_search_results(grid_search: dict) -> None:
+def best_grid_search_results(grid_search: dict) -> None:
     best_index = grid_search.best_index_
     cv_results = grid_search.cv_results_
     scorers = grid_search.scorer_
     
+    metrics, train_scores, val_scores = [], [], []
     print('Training scores')    
     for score in scorers:
         mean_train_score = cv_results['mean_train_' + score][best_index]
-        std_train_score = cv_results['std_train_' + score][best_index]        
+        std_train_score = cv_results['std_train_' + score][best_index]   
+        metrics.append(score)
+        train_scores.append(mean_train_score)
         print('{}: {:.8f} ({:.8f})'.format(score, mean_train_score, std_train_score))
         
     print('\nValidation scores')
     for score in scorers:
         mean_test_score = cv_results['mean_test_' + score][best_index]
         std_test_score = cv_results['std_test_' + score][best_index]
+        val_scores.append(mean_test_score)
         print('{}: {:.8f} ({:.8f})'.format(score, mean_test_score, std_test_score))    
+    
+    return pd.DataFrame({'metric': metrics, 'train_score': train_scores, 'val_scores': val_scores})
 
         
 # Returns size of the parameters grid size
